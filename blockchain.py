3bbddaa330983db68f39d45b59c6959b89d05bb0
Alex <100917872+Al0010@users.noreply.github.com>
2022-11-29 14:56:45 +0100

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,31 @@
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Primo esempio ---> Creazione di un hash 
+# import hashlib
+# hash = hashlib.sha256("Secret message".encode()).hexdigest() # Cambiando il dato cambia l'hash
+# print(hash)
+
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+from block import Block
+
+blockchain  = []
+
+genesis_block = Block("Chancellow on the brink.......", ["Satoshi sent 1 BTC to CerryHash",
+                                                        "Satoshi sent 10 BTC to MikyMouse",
+                                                        "Satoshi sent 100 BTC to Harry Plotter"])
+
+second_block = Block(genesis_block.block_hash, ["CerrHash sent 0.50 BTC to Frank Sinatra"])
+third_block = Block(second_block.block_hash, ["MikyMouse sent 1.50 BTC to Donald Trump"])
+
+print("Block Hash: Genesis Block")
+print(genesis_block.block_hash)
+
+print("Block Hash: Second Block")
+print(second_block.block_hash)
+
+print("Block Hash: Third Block")
+print(third_block.block_hash)
+
+# Cambiando le informazioni all' intenro del Genesis Block tutti i seguenti hash vengono modificati 
+
